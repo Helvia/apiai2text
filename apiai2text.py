1f312caf835aa2a8666de745ad2d1da4aa55f081
Davide Aversa <thek3nger@gmail.com>
2017-06-23 17:39:25 +0200

    Add some more comments

--- apiai2text.py
+++ apiai2text.py
@@ -10,7 +10,12 @@ import json
 
 from functools import reduce
 
+
 def find_text_answer(json_dict):
+    """
+    Extract information about the bot's answers and questions from 
+    the JSON dictionary passed as an argument.
+    """
     # Answers are in responses[x].messages[y].speech[z]
     # or responses[x].messages[y].title
     # title is a str
@@ -33,6 +38,10 @@ def find_text_answer(json_dict):
 
 
 def find_user_say(json_dict):
+    """
+    Extract information about the users' answers and questions from
+    the JSON dictionary passed as an argument.
+    """
     # user text is in userSays[x].data[x].text
     userSays = json_dict["userSays"]
     data = reduce(lambda x, y: x + y["data"], userSays, [])
@@ -56,11 +65,16 @@ def convert_zip_file(zip_archive_name: str):
             if not os.path.isdir(name):
                 with archive.open(name) as f:
                     json_content = json.loads(f.read())
-                    intent_entry = {"intent": name, "user_says": find_user_say(json_content), "answers": find_text_answer(json_content)}
+                    intent_entry = {"intent": name, "user_says": find_user_say(
+                        json_content), "answers": find_text_answer(json_content)}
                     all_intents.append(intent_entry)
     pretty_print(all_intents)
 
+
 def pretty_print(all_intents):
+    """
+    Generate a pretty print of the conversion output.
+    """
     for i in all_intents:
         print(f'# Intent: {i["intent"]}')
         print(f"## User Says:")
@@ -69,7 +83,6 @@ def pretty_print(all_intents):
         print("## Answers")
         for a in i["answers"]:
             print(f"\t- {a}")
-        
 
 
 if __name__ == '__main__':
