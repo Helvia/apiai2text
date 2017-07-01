202e15378ddb2e1e4ac5229f9a12b4ef5ea63e7a
Davide Aversa <thek3nger@gmail.com>
2017-07-01 12:27:35 +0200

    Add ability to dynamically load intents from API.AI
    This is done by providing the API.AI dev token to the
    --rest web interface.

--- apiai2text/rest.py
+++ apiai2text/rest.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+import requests
+import json
+from flask import Flask, request, render_template, Markup
 
-from apiai2text.data import convert_zip_file
+from apiai2text.data import convert_zip_file, APIAITextIntent, pretty_print
 import markdown2
 
 app = Flask(__name__)
@@ -8,10 +10,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', name='Hello World!')
+
 
 @app.route('/agent', methods=['GET'])
 def view_agent():
     bar = request.args.to_dict()
-    text = convert_zip_file(app.config["file"])
-    return markdown2.markdown(text)
+    if "apiai_token" in bar:
+        intents = get_all_intents(bar["apiai_token"])
+        text = pretty_print(intents)
+    else:
+        text = convert_zip_file(app.config["file"])
+    return render_template('agent.html', content=Markup(markdown2.markdown(text)))
+
+
+def get_all_intents(dev_token):
+    url = "https://api.api.ai/v1/intents?v=20150910"
+    headers = {"Authorization": "Bearer {}".format(dev_token)}
+    r = requests.get(url, headers=headers)
+    json_content = r.json()
+    intents_ids = [(x["name"], x["id"]) for x in json_content]
+    all_intents = []
+    for (name, iid) in intents_ids:
+        url = "https://api.api.ai/v1/intents/{}?v=20150910".format(iid)
+        r = requests.get(url, headers=headers)
+        all_intents.append(APIAITextIntent(name, r.json()))
+    return all_intents
